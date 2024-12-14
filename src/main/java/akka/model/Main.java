086bdaf61cb55db0d65cc24e8dd3e1e3f3aca667
wzhaoxuan <newzxuan213@gmail.com>
2024-12-14 23:33:29 +0800

    Hello World

--- src/main/java/akka/model/Main.java
+++ src/main/java/akka/model/Main.java
@@ -2,8 +2,6 @@ package akka.model;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Welcome to Java!");
+        System.out.println("Hello World");
     }
 }
