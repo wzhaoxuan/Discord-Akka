e5cdeacc98cd8b7cb1959d76019805d4e10c5e9c
chanliyangsunway <21044045@imail.sunway.edu.my>
2024-12-28 15:51:33 +0800

    deleted AI chatbot function

--- src/main/java/discord/akka/model/actors/AIChatbot.java
+++
@@ -1,8 +0,0 @@
-package discord.akka.model.actors;
-
-public class AIChatbot {
-    public String respond(String userInput) {
-
-        return "AI Bot: " + (userInput.equalsIgnoreCase("Hello") ? "Hi there! How can I assist you?" : "I'm here to help!");
-    }
-}
